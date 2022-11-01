7bf9e7e492a81bfcfd002a2a4f8db186d1487b18
Butch <ybnrml696969@yahoo.com>
2022-11-01 06:48:36 -0700

    11. Cancel orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
 	mapping(address => mapping (address => uint256)) public tokens;
 	mapping(uint256 => _Order) public orders;  
 	uint256 public orderCount;
+	mapping(uint256 => bool) public orderCancelled;
 
 	event Deposit(address token,
 	    address user, 
@@ -22,6 +23,15 @@ contract Exchange {
 		uint256 balance
 	);
 	event Order(
+		uint256 id, 
+		address user,
+		address tokenGet,
+		uint256 amountGet,
+		address tokenGive,
+		uint256 amountGive,
+		uint256 timestamp
+	);
+	event Cancel(
 		uint256 id, 
 		address user,
 		address tokenGet,
@@ -113,4 +123,35 @@ contract Exchange {
 		);
 
 	}
+
+
+	function cancelOrder(uint256 _id) public {
+
+		 _Order storage _order = orders[_id];
+
+		 require(address(_order.user) == msg.sender); 
+
+		 require(_order.id == _id);
+
+
+		 orderCancelled[_id] = true; 
+
+
+
+
+		 emit Cancel(
+			_order.id, 
+			msg.sender, 
+			_order.tokenGet, 
+			_order.amountGet,
+			_order.tokenGive,  
+			_order.amountGive, 
+			block.timestamp
+
+		);
+
+
+	}
+
+
 }
